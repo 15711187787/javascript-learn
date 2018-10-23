95ad600e02edbc8d3f04141e55c01141a0f323db
alex <alex_my@126.com>
2018-10-23 11:04:24 +0800

    test dropWhile

---
+++ learn-lodash/drop.js
@@ -0,0 +1,22 @@
+const _ = require("lodash");
+
+var users = [{
+        'user': 'barney',
+        'active': false
+    },
+    {
+        'user': 'fred',
+        'active': false
+    },
+    {
+        'user': 'pebbles',
+        'active': true
+    }
+];
+
+const result = _.dropWhile(users, function (o) {
+    return !o.active;
+});
+
+console.log(`result: ${JSON.stringify(result)}`);
+console.log(`users: ${JSON.stringify(users)}`);
